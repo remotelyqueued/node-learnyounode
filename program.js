041b3bcac3ba05a75ee433149fb8f8bad7ad4e6d
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-12 14:54:29 -0700

    Finished filtered ls

--- program.js
+++ program.js
@@ -1,12 +1,17 @@
 // https://nodeschool.io/
 // https://github.com/workshopper/learnyounode
-var fs = require('fs');
-var file = process.argv[2];
 
-fs.readFile(file, (err, contents) => {
-    if (err) {
-        return console.log(err);
-    }
-    var lines = contents.toString().split('\n').length - 1;
-    console.log(lines);
+var fs = require('fs');
+var path = require('path');
+var directory = process.argv[2];
+var ext = '.' + process.argv[3];
+
+fs.readdir(directory, (err, files) => {
+    if (err) return console.log(err);
+
+    files.forEach(file => {
+        if (path.extname(file) === ext) {
+            console.log(file);
+        }
+    });
 });
