bece7c9c21bb39e5a4e677583d8269c805c0aa0c
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-12 20:08:04 -0700

    Finished make it modular

--- module.js
+++ module.js
@@ -2,15 +2,11 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (directoryName, extension, callback) {
-    var ext = '.' + extension;
-
-    fs.readdir(directoryName, (err, files) => {
-        if (err) return callback(err);
-
-        files.forEach(file => {
-            if (path.extname(file) === ext) {
-                callback(null, file);
-            }
+    fs.readdir(directoryName, (error, files) => {
+        if (error) return callback(error);
+        files = files.filter(file => {
+            return path.extname(file) === '.' + extension;
         });
+        callback(null, files);
     });
 };
--- program.js
+++ program.js
@@ -1,12 +1,13 @@
 // https://nodeschool.io/
 // https://github.com/workshopper/learnyounode
-
 var mymodule = require('./module.js');
 
 var directory = process.argv[2];
 var extension = process.argv[3];
 
-mymodule(directory, extension, (err, data) => {
-    if (err) return console.log(err);
-    console.log(data);
+mymodule(directory, extension, (error, data) => {
+    if (error) console.log('Error:', error);
+    data.forEach(file => {
+        console.log(file);
+    });
 });
