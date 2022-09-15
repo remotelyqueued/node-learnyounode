8e7376007573cad8c180aebaa30934e21f58dc82
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-14 20:25:46 -0700

    Working on http collect

--- 8-http-collect.js
+++ 8-http-collect.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const url = process.argv[2];
+const encoding = 'utf-8';
 let data = [];
 
 http.get(url, callback).on('error', console.error);
 
 function callback(response) {
-    response.setEncoding('utf-8');
-    response.on('data', stream => data.push(stream));
+    response.setEncoding(encoding);
+    response.on('data', function (stream) {
+        data.push(stream);
+    });
     response.on('error', console.error);
     response.on('end', console.log);
 }
