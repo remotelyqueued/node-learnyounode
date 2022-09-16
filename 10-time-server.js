abf6ed9fbaaa34e0a5bd5a2d69acf200abef3edc
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-15 22:23:54 -0700

    Finished time server

--- 10-time-server.js
+++ 10-time-server.js
@@ -1,9 +1,20 @@
 const net = require('net');
-const port = process.argv[2];
+const port = Number(process.argv[2]);
+
+const zeroFill = function (i) {
+    return (i < 10 ? '0' : '') + i;
+};
 
 const server = net.createServer(function listener(socket) {
-    socket.write();
-    socket.end();
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = zeroFill(date.getMonth() + 1);
+    const day = zeroFill(date.getDate());
+    const hour = zeroFill(date.getHours());
+    const minutes = zeroFill(date.getMinutes());
+
+    socket.end(`${year}-${month}-${day} ${hour}:${minutes}\n`);
 });
 
 server.listen(port);
