da90716095377165c5ecdca616d9b1cebe0738bc
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-17 16:25:25 -0700

    Finished all the challenges

--- 13-json-api-server.js
+++ 13-json-api-server.js
@@ -1,24 +1,86 @@
 const http = require('http');
 const url = require('url');
 
-http.createServer((request, response) => {
-    request.on('error', err => {
-        console.error(err);
-        response.statusCode = 400;
-        response.end();
-    });
-    response.on('error', err => {
-        console.error(err);
-    });
+const hostname = '127.0.0.1';
+const port = Number(process.argv[2]);
 
+http.createServer((request, response) => {
     const urlObject = url.parse(request.url, true);
 
-    if (urlObject.pathname === 'api/parsetime') {
-        console.log(urlObject.search);
+    if (urlObject.pathname == '/api/parsetime') {
+        const isoString = urlObject.query.iso;
+        const date = new Date(isoString);
+
+        const returnDate = {
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds(),
+        };
+
         response.writeHead(200, {
             'Content-Type': 'application/json',
         });
-    } else if (urlObject.pathname === 'api/unixtime') {
+
+        console.log(JSON.stringify(returnDate));
+        response.end(JSON.stringify(returnDate));
+    } else if (urlObject.pathname === '/api/unixtime') {
+        const isoString = urlObject.query.iso;
+        const date = new Date(isoString);
+
+        const returnDate = {
+            unixtime: date.getTime(),
+        };
+
+        response.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+
+        console.log(JSON.stringify(returnDate));
+        response.end(JSON.stringify(returnDate));
     } else {
+        response.end('Nothing of value was received.');
     }
-}).listen(Number(process.argv[2]));
+}).listen(port);
+
+// cors errors in test browser
+// connection refused in verify
+// .listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// official solution
+// var http = require('http');
+// var url = require('url');
+
+// function parsetime(time) {
+//     return {
+//         hour: time.getHours(),
+//         minute: time.getMinutes(),
+//         second: time.getSeconds(),
+//     };
+// }
+
+// function unixtime(time) {
+//     return { unixtime: time.getTime() };
+// }
+
+// var server = http.createServer(function (req, res) {
+//     var parsedUrl = url.parse(req.url, true);
+//     var time = new Date(parsedUrl.query.iso);
+//     var result;
+
+//     if (/^\/api\/parsetime/.test(req.url)) {
+//         result = parsetime(time);
+//     } else if (/^\/api\/unixtime/.test(req.url)) {
+//         result = unixtime(time);
+//     }
+
+//     if (result) {
+//         res.writeHead(200, { 'Content-Type': 'application/json' });
+//         res.end(JSON.stringify(result));
+//     } else {
+//         res.writeHead(404);
+//         res.end();
+//     }
+// });
+// server.listen(Number(process.argv[2]));
