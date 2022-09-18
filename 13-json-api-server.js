b8c5a3394f1d87ba0d050a76b5eb09e1e1bcaf6f
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-17 18:25:53 -0700

    Cleaned up solution

--- 13-json-api-server.js
+++ 13-json-api-server.js
@@ -1,86 +1,44 @@
 const http = require('http');
 const url = require('url');
-
-const hostname = '127.0.0.1';
 const port = Number(process.argv[2]);
+const hostname = '127.0.0.1';
 
-http.createServer((request, response) => {
-    const urlObject = url.parse(request.url, true);
-
-    if (urlObject.pathname == '/api/parsetime') {
-        const isoString = urlObject.query.iso;
-        const date = new Date(isoString);
-
-        const returnDate = {
-            hour: date.getHours(),
-            minute: date.getMinutes(),
-            second: date.getSeconds(),
-        };
-
-        response.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-
-        console.log(JSON.stringify(returnDate));
-        response.end(JSON.stringify(returnDate));
-    } else if (urlObject.pathname === '/api/unixtime') {
-        const isoString = urlObject.query.iso;
-        const date = new Date(isoString);
-
-        const returnDate = {
-            unixtime: date.getTime(),
-        };
-
-        response.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-
-        console.log(JSON.stringify(returnDate));
-        response.end(JSON.stringify(returnDate));
+http.createServer((req, res) => {
+    const parsedURL = url.parse(req.url, true);
+    if (parsedURL.pathname === '/api/parsetime') {
+        let date = new Date(parsedURL.query.iso);
+        date = parsetime(date);
+        res.writeHead(200, setheaders());
+        res.end(JSON.stringify(date));
+    } else if (parsedURL.pathname === '/api/unixtime') {
+        let date = new Date(parsedURL.query.iso);
+        date = unixtime(date);
+        res.writeHead(200, setheaders());
+        res.end(JSON.stringify(date));
     } else {
-        response.end('Nothing of value was received.');
+        res.end('Nothing of value was received.');
     }
-}).listen(port);
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
-// cors errors in test browser
-// connection refused in verify
-// .listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+function parsetime(time) {
+    return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds(),
+    };
+}
 
-// official solution
-// var http = require('http');
-// var url = require('url');
+function unixtime(time) {
+    return { unixtime: time.getTime() };
+}
 
-// function parsetime(time) {
-//     return {
-//         hour: time.getHours(),
-//         minute: time.getMinutes(),
-//         second: time.getSeconds(),
-//     };
-// }
-
-// function unixtime(time) {
-//     return { unixtime: time.getTime() };
-// }
-
-// var server = http.createServer(function (req, res) {
-//     var parsedUrl = url.parse(req.url, true);
-//     var time = new Date(parsedUrl.query.iso);
-//     var result;
-
-//     if (/^\/api\/parsetime/.test(req.url)) {
-//         result = parsetime(time);
-//     } else if (/^\/api\/unixtime/.test(req.url)) {
-//         result = unixtime(time);
-//     }
-
-//     if (result) {
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify(result));
-//     } else {
-//         res.writeHead(404);
-//         res.end();
-//     }
-// });
-// server.listen(Number(process.argv[2]));
+function setheaders() {
+    return {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Max-Age': 86400,
+    };
+}
